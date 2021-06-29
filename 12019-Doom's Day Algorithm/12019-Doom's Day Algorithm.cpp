bb6f42d7a89d7d2c0c563204e61433d2061a4b5c
ricky-696 <62836757+ricky-696@users.noreply.github.com>
2021-06-29 16:35:55 +0800

    2021/06/29

---
+++ 12019-Doom's Day Algorithm/12019-Doom's Day Algorithm.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    string date[] = { "Saturday","Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday" };
+    int day[] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+    ifstream file;
+    file.open("in1.txt",ios::in);
+    string r = "";
+    getline(file,r);
+    while (!file.eof())
+    {
+        int m, d;
+        file >> m;
+        file >> d;
+        for (int i = 0; i < m - 1; i++) 
+        {
+            d = d + day[i];
+        }
+
+       cout << date[(d - 1) % 7] << endl;
+    }
+}
+
+
